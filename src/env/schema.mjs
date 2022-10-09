045ef17dfe1b23f22f3043837412362c1a6b2825
nelson frank <nelsonfr78@outlook.com>
2022-10-09 17:17:59 +0300

    fix: fix env variable error

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -8,7 +8,7 @@ import { z } from "zod";
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
-  NEXTAUTH_SECRET: z.string(),
+  NEXTAUTH_SECRET: z.string().optional(),
   NEXTAUTH_URL: z.string().url(),
   JWT_TOKEN_SECRET: z.string(),
 });
